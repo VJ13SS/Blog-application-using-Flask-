740099d8c0e3a8ea63d97bbf4eee4025c889fe2f
VJ13SS <135973734+VJ13SS@users.noreply.github.com>
2024-06-15 21:39:00 +0530

    Updated Blog application.py

--- Blog application.py
+++ Blog application.py
@@ -1,4 +1,4 @@
-#importing the necessary libraries
+###importing the necessary libraries
 from flask import Flask,render_template, redirect, url_for,request,session,flash
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -8,7 +8,7 @@ from datetime import timedelta
 app= Flask(__name__)
 app.secret_key = ('J06x1r@13')
 
-#comfigurations for sqlalchemy
+#configurations for sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_info.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -33,6 +33,9 @@ class User(db.Model):
 	
 	liked_by = db.relationship('Likes',backref = 'user',passive_deletes = True)
 	
+	#user followers
+	followers = db.relationship('Follows',backref = 'user',passive_deletes = True)#following the user
+		
 	
 #Posts Table
 class Post(db.Model):
@@ -46,8 +49,8 @@ class Post(db.Model):
 	post_comments = db.relationship('Comments', backref = 'post',passive_deletes = True)
 	
 	post_likes = db.relationship('Likes',backref = 'post',passive_deletes = True)
-		
-		
+	
+	
 #comments Table
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,7 +67,18 @@ class Likes(db.Model):
 	
 	id = db.Column(db.Integer,primary_key = True)
 	author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+	
 	liked_post = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
+	
+#Follows Table
+class Follows(db.Model):
+	id = db.Column(db.Integer,primary_key = True)
+	
+	#who is following
+	follower = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+	
+	#whom is being followed(post owner)
+	following = db.Column(db.String(25),nullable = False)
 	
 #permanent session
 app.permanent_session_lifetime = timedelta(minutes = 5)
@@ -167,8 +181,11 @@ def home():
 		
 		#to access all the posts of the obtained user
 		posts = Post.query.filter_by(author = user.id).all()
+		
+		user_followers = user.followers
+		length_F = len(user_followers)
 			
-		return render_template('Home_FP.html',current_user = user,display_posts = posts[: : -1])
+		return render_template('Home_FP.html',current_user = user,display_posts = posts[: : -1],total_followers = length_F)
 
 #edit profile
 @app.route('/editprofile/<id>',methods =['GET','POST'])
@@ -218,6 +235,7 @@ def create_post():
 			db.session.add(new_post)
 			db.session.commit()
 	return render_template('CreatePosts_FP.html',default = '')
+	
 
 #viewing user posts
 @app.route('/viewposts',methods =['GET','POST'])
@@ -225,26 +243,26 @@ def viewposts():
 	
 	#taking all post,commemts
 	posts = Post.query.all()
-	comments = Comments.query.all()
 	
 	#posts[ : :-1] --> to view the latest posts first
-	return render_template('Viewposts_FP.html',user_posts = posts[::-1])
+	return render_template('Viewposts_FP.html',user_posts = posts[::-1],search = 0)#search is 0 means to display the user's post's'
+	
 
 #search user posts
 @app.route('/search-user',methods = ['GET','POST'])
 def search_user():
 	if request.method == 'POST':
-		searched_user = request.form['find_user']
+		searched_user = request.form['find_user'].strip()
 		
 		if not searched_user:
 			flash('User dosent exists...!')
 		
 		else:
+			#to get the searched user
 			user = User.query.filter_by(name = searched_user).first()
 			
 			if user:
-				posts = Post.query.filter_by(author = user.id).all()
-				return render_template('Viewposts_FP.html',user_posts = posts[::-1])
+				return render_template('Viewposts_FP.html',todisplay = user,search = 1)#search is 1means to display the user profile
 				
 			else:
 				flash('User dosent exists...!')
@@ -275,6 +293,39 @@ def likepost(id):
 		db.session.commit()
 		
 	return redirect('/postdetails/' + id )
+	
+	
+#following user
+@app.route('/follow-user/<id>')
+def connect(id):
+	
+	#the person who tends to follow
+	user = session['user']
+	visited_user = User.query.filter_by(name = user).first()
+	
+	profile_owner = User.query.filter_by(id = id).first()
+	
+	
+	if not(visited_user):
+		flash('User dosent Exists')
+		return redirect(url_for('viewposts'))
+		
+	#checks if the visited user follows/is connected with the account holder
+	isFollowing = Follows.query.filter_by(follower = id,following = visited_user.name).first()
+	
+	#if follows/connected already
+	if isFollowing:
+		
+		db.session.delete(isFollowing)
+	
+	else:
+		newFollow = Follows(follower = id,following = user)
+		db.session.add(newFollow)
+		
+	db.session.commit()
+	
+	return redirect('/user/' + id)
+	
 
 #edit a post
 @app.route('/editpost/<id>',methods = ['GET', 'POST'])
@@ -299,11 +350,30 @@ def viewuser(id):
 	
 	user = User.query.filter_by(id = id).first()
 	
+	#the one who inspects or visits the profile
+	visited = session['user']
+	visited_user = User.query.filter_by(name = visited).first()
+	
+	if not visited_user:
+		flash('User not Online')
+		return redirect('/')
+	
+	#checks if the visited user follows the account holder
+	isFollowing = Follows.query.filter_by(follower = id,following = visited_user.name).first()
+	
+	if isFollowing:
+		following = 1#user already follows the account holder
+	else:
+		following = 0
+	
 	#to get all the posts of the user
 	user_posts = user.posts#we do have a post column in user table which is a foreign key realting the post table
-	length = len(user_posts)
+	length_P = len(user_posts)
 	
-	return render_template('UserPosts_FP.html',view_user = user,view_user_posts = user_posts[: : -1],total_posts = length)
+	user_followers = user.followers
+	length_F = len(user_followers)
+	
+	return render_template('UserPosts_FP.html',profile_viewed = visited_user.name,view_user = user,view_user_posts = user_posts[: : -1],total_posts = length_P,total_followers = length_F,Follow =following)
 
 #delete a post
 @app.route('/deletepost/<id>')#<id> is the dynamic variable passed (check Home_FP.html to know how it is passed)
@@ -316,6 +386,7 @@ def deletepost(id):
 		db.session.commit()
 	
 	return redirect(url_for('home'))
+
 
 #view a post,likes,add comments
 @app.route('/postdetails/<id>',methods = ['POST','GET'])
@@ -345,6 +416,7 @@ def viewpost(id):
 	#getting the total number of likes
 	post_likes = Likes.query.filter_by(liked_post = id).all()
 	length_l = len(post_likes)
+	
 	
 	#checking if user liked the post or not
 	liked_by = User.query.filter_by(name = user).first()
